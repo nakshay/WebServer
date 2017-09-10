08a2a327dbf5f840ba7aaaf4fe6ca9104cae74e5
Akshay Naik <inbox.akshaynaik@gmail.com>
2017-09-10 18:31:53 +0530

    first request received

--- webserver.c
+++ webserver.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "libuv/include/uv.h"
 
 static uv_tcp_t server;
 
 
 
-void on_connection (uv_stream_t* server, int status) {
+void on_connection (uv_stream_t* handle, int status) {
 
+	assert(handle == &server);
+	printf("on_connection called\n");
 
 }
 
@@ -18,13 +21,13 @@ int main() {
 
   	struct sockaddr_in addr;
 
-  	uv_ip4_addr("0.0.0.0", 0, &addr);
+  	uv_ip4_addr("0.0.0.0", 8000, &addr);
 
     uv_tcp_bind(&server, (const struct sockaddr*)&addr, 0);
 
 	printf("Hello world !\n");
 
-	int r = uv_listen((uv_stream_t*) &server, 8000, on_connection);
+	int r = uv_listen((uv_stream_t*) &server, 128, on_connection);
     if (r) {
         fprintf(stderr, "Listen error %s\n", uv_strerror(r));
         return -1;
